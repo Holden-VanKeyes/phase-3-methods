cb03126f266f3c10f751ef2eb37270a29bf78b77
Abram Shook <ajshook77@gmail.com>
2022-06-05 21:24:28 -0500

    commit tree bab7ebef9e29a4ce2990ecf0cc1e4372a73e2ef3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
-# Your code here!
+
+
+def greet_programmer
+     puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet('Naureen')
+
+
+
+ def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+ end
+ greet_with_default
+
+ 
+
+
+  def add(num1, num2)
+    num1 + num2
+  end
+ 
+  add(0,0)
+ 
+
+ # function halve(number) {
+ # if (typeof number !== "number") return null;
+
+ # return number / 2;
+
+ def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+end
+halve(0)
+
